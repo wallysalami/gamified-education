7d40ed7be12c8a609a2fb51b4fe58aa789b3a86c
Jan Siqueira <janksiqueira@gmail.com>
2017-08-12 21:22:05 -0300

    Refactor the function that reads .env file

--- gamifiededucation/wsgi.py
+++ gamifiededucation/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+from gamifiededucation.helper import load_to_environment
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+local_env_path = os.path.join(base_dir, '../local.env')
+load_to_environment(local_env_path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gamifiededucation.settings")
 
--- manage.py
+++ manage.py
@@ -1,36 +1,15 @@
 #!/usr/bin/env python
 import os
 import sys
-import re
 
-def read_env():
-    """Pulled from Honcho code with minor updates, reads local default
-    environment variables from a .env file located in the project root
-    directory.
-    """
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    local_env_path = os.path.join(base_dir, 'local.env')
-    try:
-        with open(local_env_path) as f:
-            content = f.read()
-    except IOError:
-        content = ''
-
-    for line in content.splitlines():
-        m1 = re.match(r'\A([A-Za-z_0-9]+)=(.*)\Z', line)
-        if m1:
-            key, val = m1.group(1), m1.group(2)
-            m2 = re.match(r"\A'(.*)'\Z", val)
-            if m2:
-                val = m2.group(1)
-            m3 = re.match(r'\A"(.*)"\Z', val)
-            if m3:
-                val = re.sub(r'\\(.)', r'\1', m3.group(1))
-            os.environ.setdefault(key, val)
+from gamifiededucation.helper import load_to_environment
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gamifiededucation.settings")
-    read_env()
+    
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    local_env_path = os.path.join(base_dir, 'local.env')
+    load_to_environment(local_env_path)
 
     try:
         from django.core.management import execute_from_command_line
