aa908abcecdcd2f5831271acc9f5c956b97ccb60
Jan K. S <developer@janks.software>
2024-01-29 19:06:39 -0300

    Fix login

--- course/backend.py
+++ course/backend.py
@@ -2,17 +2,20 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 
 class UsernameOrEmailBackend(ModelBackend):
-    def authenticate(self, username=None, password=None, **kwargs):
+    def authenticate(self, request, username=None, password=None, **kwargs):
         UserModel = get_user_model()
         try:
-            user = UserModel.objects.get(username=username)
+            user = UserModel._default_manager.get(username=username)
         except UserModel.DoesNotExist:
             try:
-                user = UserModel.objects.get(email=username)
+                user = UserModel._default_manager.get(email=username.lower())
             except UserModel.DoesNotExist:
+                # Run the default password hasher once to reduce the timing
+                # difference between an existing and a nonexistent user (#20760).
+                UserModel().set_password(password)
                 return None
         
-        if user.check_password(password):
+        if user.check_password(password) and self.user_can_authenticate(user):
             return user
 
         return None
