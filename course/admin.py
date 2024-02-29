01d9cf186b8d9b89c2423e4c6ecbbe422603d9fa
Jan K. S <developer@janks.software>
2024-02-29 20:14:13 -0300

    Add custom permissions for EnrollmentAdmin
    I forgot this one last time, ops.

--- course/admin.py
+++ course/admin.py
@@ -370,10 +370,9 @@ def last_login_formatted_for_enrolment(self):
     
 last_login_formatted_for_enrolment.short_description = _('Last Login')
 
-class EnrollmentAdmin(BasicAdmin):
+class EnrollmentAdmin(InstructorAdminBase):
     inlines = [SimpleGradeInline]
     list_display = ('student', 'id_number', 'course_class', 'total_score', 'lost_lives', last_login_formatted_for_enrolment)
-    list_filter = ('course_class',)
     ordering = ('-course_class__start_date', 'student__full_name')
     search_fields = ('student__full_name',)
     
